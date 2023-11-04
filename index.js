54f60ebcbbd1ef71980f204e479ed02921315a47
Shihab Hasan <shihabbd018@gmail.com>
2023-11-04 22:42:06 +0600

    added api for top selling foods

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
+
 
 const port = 9000;
 
@@ -11,6 +13,46 @@ app.use(express.json());
 
 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kitiq8p.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+
+
+    const foodsCollection = client.db('dineinDB').collection('foods');
+
+
+    app.get("/topSelling", async (req, res) => {
+        try {
+            const result = await foodsCollection.find().sort({ purchaseCount: -1 }).limit(6).toArray();
+            res.send(result);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    })
+
+
+    try {
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
